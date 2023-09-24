3ca6a1e6b46868b2608d9a29e018ec3ff864d541
Obianuju Enekebe <obianujuenekebe@Obianujus-Air.home>
2023-09-23 22:53:17 -0400

    updated code

---
+++ cosmic_weight/static/cosmic_weight/js/script.js
@@ -0,0 +1,162 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Mercury");
+    let theMenu = document.getElementById("Mercury-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Venus");
+    let theMenu = document.getElementById("Venus-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Earth");
+    let theMenu = document.getElementById("Earth-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Mars");
+    let theMenu = document.getElementById("Mars-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Jupiter");
+    let theMenu = document.getElementById("Jupiter-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Saturn");
+    let theMenu = document.getElementById("Saturn-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Uranus");
+    let theMenu = document.getElementById("Uranus-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Neptune");
+    let theMenu = document.getElementById("Neptune-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("fired");
+    let myImg = document.getElementById("Pluto");
+    let theMenu = document.getElementById("Pluto-my-menu");
+    let isVisible = false;
+    myImg.addEventListener("click", () => {
+        // On click, open a menu that displays Venus data
+        console.log("clicked");
+        if(isVisible) {
+            theMenu.style.display = "none";
+            isVisible = false;
+        }
+        else {
+            theMenu.style.display = "flex";
+            isVisible = true;
+        }
+    });
+});
